88bf98c81e3ad4bd30d71bdbb74708746fe94034
PuddingNGV <puddingngv@gmail.com>
2021-04-23 16:09:31 +0300

    time

--- app/src/main/java/com/example/smokedrop/SmokeDrop.kt
+++ app/src/main/java/com/example/smokedrop/SmokeDrop.kt
@@ -1,20 +1,22 @@
 package com.example.smokedrop
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.smokedrop.info.InfoFragment
 import com.example.smokedrop.map.MapFragment
 import com.example.smokedrop.settings.SettingsFragment
 import com.example.smokedrop.time.TimeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class SmokeDrop : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_app)
 
-        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnNavigationItemSelectedListener(navigationItemSelectedListener())
+        setContentView(R.layout.activity_app)
         setSupportActionBar(findViewById(R.id.toolbar))
+        findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnNavigationItemSelectedListener(navigationItemSelectedListener())
 
         if (savedInstanceState == null) {
             val fragment = TimeFragment()
@@ -23,34 +25,35 @@ class SmokeDrop : AppCompatActivity() {
         }
 
     }
+
     private fun navigationItemSelectedListener() = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
         when (menuItem.itemId) {
             R.id.timeFragment -> {
                 val fragment = TimeFragment()
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, fragment, fragment.javaClass.getSimpleName())
-                    .commit()
+                        .replace(R.id.fragment_container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.infoFragment -> {
                 val fragment = InfoFragment()
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, fragment, fragment.javaClass.getSimpleName())
-                    .commit()
+                        .replace(R.id.fragment_container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.mapFragment -> {
                 val fragment = MapFragment()
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, fragment, fragment.javaClass.getSimpleName())
-                    .commit()
+                        .replace(R.id.fragment_container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.settingsFragment -> {
                 val fragment = SettingsFragment()
                 supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, fragment, fragment.javaClass.getSimpleName())
-                    .commit()
+                        .replace(R.id.fragment_container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
--- app/src/main/java/com/example/smokedrop/time/TimeFragment.kt
+++ app/src/main/java/com/example/smokedrop/time/TimeFragment.kt
@@ -1,16 +1,22 @@
 package com.example.smokedrop.time
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.smokedrop.R
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private val timeless = null
 
 /**
  * A simple [Fragment] subclass.
@@ -31,10 +37,27 @@ class TimeFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_time, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val timeless = view!!.findViewById(R.id.textTime) as TextView
+        val timeles = view!!.findViewById(R.id.textTimeLast) as TextView
+
+
+        var current = LocalDateTime.now().plusHours(1L)
+        val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+        var formatted = current.format(formatter)
+        timeless.setText(formatted)
+
+        current= LocalDateTime.now()
+        formatted = current.format(formatter)
+        timeles.setText(formatted)
+
     }
 }
