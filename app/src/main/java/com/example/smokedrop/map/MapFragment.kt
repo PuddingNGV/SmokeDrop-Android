c4219b57ca54cc2808c58f0f4d07ae6958337836
PuddingNGV <puddingngv@gmail.com>
2021-05-26 16:33:08 +0300

    codeStyle

--- app/src/main/java/com/example/smokedrop/map/MapFragment.kt
+++ app/src/main/java/com/example/smokedrop/map/MapFragment.kt
@@ -55,102 +55,70 @@ class MapFragment : Fragment() {
         mapController.setZoom(16)
         val startPoint = GeoPoint(55.707772, 52.356435)
         mapController.setCenter(startPoint)
+        map.overlayManager.tilesOverlay.setColorFilter(filterMaps())
         map.onResume()
-
-
-
-
-            val inverseMatrix = ColorMatrix(
-                floatArrayOf(
-                    -1.0f, 0.0f, 0.0f, 0.0f, 255f,
-                    0.0f, -1.0f, 0.0f, 0.0f, 255f,
-                    0.0f, 0.0f, -1.0f, 0.0f, 255f,
-                    0.0f, 0.0f, 0.0f, 1.0f, 0.0f
-                )
-            )
-            val destinationColor: Int = Color.parseColor("#FF2A2A2A")
-            val lr: Float = (255.0f - Color.red(destinationColor)) / 255.0f
-            val lg: Float = (255.0f - Color.green(destinationColor)) / 255.0f
-            val lb: Float = (255.0f - Color.blue(destinationColor)) / 255.0f
-            val grayscaleMatrix = ColorMatrix(
-                floatArrayOf(
-                    lr, lg, lb, 0f, 0f,  //
-                    lr, lg, lb, 0f, 0f,  //
-                    lr, lg, lb, 0f, 0f, 0f, 0f, 0f, 0f, 255f
-                )
-            )
-            grayscaleMatrix.preConcat(inverseMatrix)
-            val dr: Int = Color.red(destinationColor)
-            val dg: Int = Color.green(destinationColor)
-            val db: Int = Color.blue(destinationColor)
-            val drf = dr / 255f
-            val dgf = dg / 255f
-            val dbf = db / 255f
-            val tintMatrix = ColorMatrix(
-                floatArrayOf(
-                    drf,
-                    0f,
-                    0f,
-                    0f,
-                    0f,
-                    0f,
-                    dgf,
-                    0f,
-                    0f,
-                    0f,
-                    0f,
-                    0f,
-                    dbf,
-                    0f,
-                    3f,
-                    0.5f,
-                    0f,
-                    1f,
-                    1f,
-                    0f
-                )
-            )
-            tintMatrix.preConcat(grayscaleMatrix)
-            val lDestination = drf * lr + dgf * lg + dbf * lb
-            val scale = 1f - lDestination
-            val translate = 1 - scale * 1.0f
-            val scaleMatrix = ColorMatrix(
-                floatArrayOf(
-                    scale,
-                    0f,
-                    0f,
-                    0f,
-                    dr * translate,
-                    0f,
-                    scale,
-                    0f,
-                    0f,
-                    dg * translate,
-                    0f,
-                    0f,
-                    scale,
-                    0f,
-                    db * translate,
-                    0f,
-                    0f,
-                    0f,
-                    1f,
-                    0f
-                )
-            )
-            scaleMatrix.preConcat(tintMatrix)
-            val filter = ColorMatrixColorFilter(scaleMatrix)
-            map.overlayManager.tilesOverlay.setColorFilter(filter)
-
-
-
-
-
 
     }
 
     override fun onPause() {
         super.onPause()
         map.onPause()
+    }
+
+
+
+
+
+
+    private fun filterMaps():ColorMatrixColorFilter {
+        val inverseMatrix = ColorMatrix(
+            floatArrayOf(
+                -1.0f, 0.0f, 0.0f, 0.0f, 255f,
+                0.0f, -1.0f, 0.0f, 0.0f, 255f,
+                0.0f, 0.0f, -1.0f, 0.0f, 255f,
+                0.0f, 0.0f, 0.0f, 1.0f, 0.0f
+            )
+        )
+        val destinationColor: Int = Color.parseColor("#FF2A2A2A")
+        val lr: Float = (255.0f - Color.red(destinationColor)) / 255.0f
+        val lg: Float = (255.0f - Color.green(destinationColor)) / 255.0f
+        val lb: Float = (255.0f - Color.blue(destinationColor)) / 255.0f
+        val grayscaleMatrix = ColorMatrix(
+            floatArrayOf(
+                lr, lg, lb, 0f, 0f,  //
+                lr, lg, lb, 0f, 0f,  //
+                lr, lg, lb, 0f, 0f, 0f, 0f, 0f, 0f, 255f
+            )
+        )
+        grayscaleMatrix.preConcat(inverseMatrix)
+        val dr: Int = Color.red(destinationColor)
+        val dg: Int = Color.green(destinationColor)
+        val db: Int = Color.blue(destinationColor)
+        val drf = dr / 255f
+        val dgf = dg / 255f
+        val dbf = db / 255f
+        val tintMatrix = ColorMatrix(
+            floatArrayOf(
+                drf, 0f, 0f, 0f, 0f, 0f,
+                dgf, 0f, 0f, 0f, 0f, 0f,
+                dbf, 0f, 3f, 0.5f, 0f, 1f, 1f, 0f
+            )
+        )
+        tintMatrix.preConcat(grayscaleMatrix)
+        val lDestination = drf * lr + dgf * lg + dbf * lb
+        val scale = 1f - lDestination
+        val translate = 1 - scale * 1.0f
+        val scaleMatrix = ColorMatrix(
+            floatArrayOf(
+                scale, 0f, 0f, 0f,
+                dr * translate, 0f,
+                scale, 0f, 0f,
+                dg * translate, 0f, 0f,
+                scale, 0f,
+                db * translate, 0f, 0f, 0f, 1f, 0f
+            )
+        )
+        scaleMatrix.preConcat(tintMatrix)
+        return ColorMatrixColorFilter(scaleMatrix)
     }
 }
