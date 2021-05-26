3c46d576e9f827d0fb55eb75d335fc5d1729fc90
PuddingNGV <puddingngv@gmail.com>
2021-05-26 16:05:06 +0300

    osm added

--- app/src/main/java/com/example/smokedrop/map/MapFragment.kt
+++ app/src/main/java/com/example/smokedrop/map/MapFragment.kt
@@ -1,11 +1,21 @@
 package com.example.smokedrop.map
 
+import android.graphics.Color
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.smokedrop.R
+import org.osmdroid.config.Configuration.*
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.TilesOverlay
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,16 +28,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class MapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+    private lateinit var map: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -38,6 +44,113 @@ class MapFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
-    companion object {
+    override fun onStart() {
+        super.onStart()
+        getInstance().load(activity, PreferenceManager.getDefaultSharedPreferences(activity))
+        map = requireView().findViewById(R.id.mapview)
+        map.setTileSource(TileSourceFactory.HIKEBIKEMAP)
+        map.setMultiTouchControls(true)
+        val mapController = map.controller
+        //map.overlayManager.tilesOverlay.setColorFilter(TilesOverlay.INVERT_COLORS)
+        mapController.setZoom(16)
+        val startPoint = GeoPoint(55.707772, 52.356435)
+        mapController.setCenter(startPoint)
+        map.onResume()
+
+
+
+
+            val inverseMatrix = ColorMatrix(
+                floatArrayOf(
+                    -1.0f, 0.0f, 0.0f, 0.0f, 255f,
+                    0.0f, -1.0f, 0.0f, 0.0f, 255f,
+                    0.0f, 0.0f, -1.0f, 0.0f, 255f,
+                    0.0f, 0.0f, 0.0f, 1.0f, 0.0f
+                )
+            )
+            val destinationColor: Int = Color.parseColor("#FF2A2A2A")
+            val lr: Float = (255.0f - Color.red(destinationColor)) / 255.0f
+            val lg: Float = (255.0f - Color.green(destinationColor)) / 255.0f
+            val lb: Float = (255.0f - Color.blue(destinationColor)) / 255.0f
+            val grayscaleMatrix = ColorMatrix(
+                floatArrayOf(
+                    lr, lg, lb, 0f, 0f,  //
+                    lr, lg, lb, 0f, 0f,  //
+                    lr, lg, lb, 0f, 0f, 0f, 0f, 0f, 0f, 255f
+                )
+            )
+            grayscaleMatrix.preConcat(inverseMatrix)
+            val dr: Int = Color.red(destinationColor)
+            val dg: Int = Color.green(destinationColor)
+            val db: Int = Color.blue(destinationColor)
+            val drf = dr / 255f
+            val dgf = dg / 255f
+            val dbf = db / 255f
+            val tintMatrix = ColorMatrix(
+                floatArrayOf(
+                    drf,
+                    0f,
+                    0f,
+                    0f,
+                    0f,
+                    0f,
+                    dgf,
+                    0f,
+                    0f,
+                    0f,
+                    0f,
+                    0f,
+                    dbf,
+                    0f,
+                    3f,
+                    0.5f,
+                    0f,
+                    1f,
+                    1f,
+                    0f
+                )
+            )
+            tintMatrix.preConcat(grayscaleMatrix)
+            val lDestination = drf * lr + dgf * lg + dbf * lb
+            val scale = 1f - lDestination
+            val translate = 1 - scale * 1.0f
+            val scaleMatrix = ColorMatrix(
+                floatArrayOf(
+                    scale,
+                    0f,
+                    0f,
+                    0f,
+                    dr * translate,
+                    0f,
+                    scale,
+                    0f,
+                    0f,
+                    dg * translate,
+                    0f,
+                    0f,
+                    scale,
+                    0f,
+                    db * translate,
+                    0f,
+                    0f,
+                    0f,
+                    1f,
+                    0f
+                )
+            )
+            scaleMatrix.preConcat(tintMatrix)
+            val filter = ColorMatrixColorFilter(scaleMatrix)
+            map.overlayManager.tilesOverlay.setColorFilter(filter)
+
+
+
+
+
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
     }
 }
